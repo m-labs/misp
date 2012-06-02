a8b967b870b20da8b17498ce2680f75e5a76398a
Sebastien Bourdeauducq <sebastien@milkymist.org>
2012-06-03 00:22:12 +0200

    Lua filesystem demo

--- main.c
+++ main.c
@@ -18,6 +18,25 @@ static void test_lua(void)
 	L = luaL_newstate();
 	luaL_openlibs(L);
 	luaL_dostring(L, "for i=10,1,-1 do print(i) end\n");
+	luaL_dostring(L, "function attrdir (path)\n"
+"    for file in lfs.dir(path) do\n"
+"        if file ~= \".\" and file ~= \"..\" then\n"
+"            local f = path..'/'..file\n"
+"            print (\"\t \"..f)\n"
+"            local attr = lfs.attributes (f)\n"
+"            assert (type(attr) == \"table\")\n"
+"            if attr.mode == \"directory\" then\n"
+"                attrdir (f)\n"
+"            else\n"
+"                for name, value in pairs(attr) do\n"
+"                    print (name, value)\n"
+"                end\n"
+"            end\n"
+"        end\n"
+"    end\n"
+"end\n"
+"\n"
+"attrdir (\"/\")\n");
 	lua_close(L);
 }
 
